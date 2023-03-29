c675119978514aef350b7c58d8852118c566bc0f
Justin Liang <justinliang1020@gmail.com>
2023-03-28 22:19:46 -0500

    paths table

--- src/pages/plugin/[name].tsx
+++ src/pages/plugin/[name].tsx
@@ -27,6 +27,45 @@ const PluginPage: React.FC<PluginPageProps> = ({ plugin }) => {
         &larr; Back
       </button>
       <PluginCard plugin={plugin} />
+
+      <div className="flex flex-col justify-center items-center p-16 mt-8 gap-8 bg-white rounded-lg shadow-md border-2">
+        <div className="flex flex-row gap-8 items-center">
+          <div>
+            {/* <h1 className="text-5xl font-bold ">{api.info.title}</h1>
+            <p className="py-6">{api.info.description}</p> */}
+            <a className="daisy-link daisy-link-primary" href="https://server.shop.app/openai/v1/api.json">Link to API Schema</a>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="daisy-table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Path</th>
+                  <th>Name</th>
+                  <th>Summary</th>
+                  {/* <th>Parameters</th> */}
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <td>/openai/search</td>
+                  <td>{api.paths["/openai/search"].get.operationId}</td>
+                  <td>{api.paths["/openai/search"].get.summary}</td>
+                  {/* <td className="overflow-x-scroll">{api.paths["/openai/search"].get.parameters.map((param) => param.name).join(", ")}</td> */}
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <td>/openai/details</td>
+                  <td>{api.paths["/openai/details"].get.operationId}</td>
+                  <td>{api.paths["/openai/details"].get.summary}</td>
+                  {/* <td className="overflow-x-scroll">{api.paths["/openai/details"].get.parameters.map((param) => param.name).join(", ")}</td> */}
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -46,3 +85,159 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 export default PluginPage;
+
+// todo: remove when we have a better way to get the API schema
+const api = {
+  "openapi": "3.0.1",
+  "info": {
+    "title": "Shop",
+    "description": "Search for millions of products from the world's greatest brands.",
+    "version": "v1"
+  },
+  "servers": [
+    {
+      "url": "https://server.shop.app"
+    }
+  ],
+  "paths": {
+    "/openai/search": {
+      "get": {
+        "operationId": "search",
+        "summary": "Search for products",
+        "parameters": [
+          {
+            "in": "query",
+            "name": "query",
+            "description": "Query string to search for items.",
+            "required": false,
+            "schema": {
+              "type": "string"
+            }
+          },
+          {
+            "in": "query",
+            "name": "price_min",
+            "description": "The minimum price to filter by.",
+            "required": false,
+            "schema": {
+              "type": "number"
+            }
+          },
+          {
+            "in": "query",
+            "name": "price_max",
+            "description": "The maximum price to filter by.",
+            "required": false,
+            "schema": {
+              "type": "number"
+            }
+          },
+          {
+            "in": "query",
+            "name": "similar_to_id",
+            "description": "A product id that you want to find similar products for. (Only include one)",
+            "required": false,
+            "schema": {
+              "type": "string"
+            }
+          },
+          {
+            "in": "query",
+            "name": "num_results",
+            "description": "How many results to return. Defaults to 5. It can be a number between 1 and 10.",
+            "required": false,
+            "schema": {
+              "type": "string"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/searchResponse"
+                }
+              }
+            }
+          },
+          "503": {
+            "description": "Service Unavailable"
+          }
+        }
+      }
+    },
+    "/openai/details": {
+      "get": {
+        "operationId": "details",
+        "summary": "Return more details about a list of products.",
+        "parameters": [
+          {
+            "in": "query",
+            "name": "ids",
+            "description": "Comma separated list of product ids",
+            "required": true,
+            "schema": {
+              "type": "string"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/searchResponse"
+                }
+              }
+            }
+          },
+          "503": {
+            "description": "Service Unavailable"
+          }
+        }
+      }
+    }
+  },
+  "components": {
+    "schemas": {
+      "searchResponse": {
+        "type": "object",
+        "properties": {
+          "results": {
+            "type": "array",
+            "items": {
+              "type": "object",
+              "properties": {
+                "title": {
+                  "type": "string",
+                  "description": "The title of the product"
+                },
+                "price": {
+                  "type": "number",
+                  "format": "string",
+                  "description": "The price of the product"
+                },
+                "currency_code": {
+                  "type": "string",
+                  "description": "The currency that the price is in"
+                },
+                "url": {
+                  "type": "string",
+                  "description": "The url of the product page for this product"
+                },
+                "description": {
+                  "type": "string",
+                  "description": "The description of the product"
+                }
+              },
+              "description": "The list of products matching the search"
+            }
+          }
+        }
+      }
+    }
+  }
+}
